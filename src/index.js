ef33846fa6555cdaf75ead5c9e461124226d4eeb
7jcdev <7jaydev@protonmail.com>
2024-11-15 06:28:15 -0600

    Added cors

--- src/index.js
+++ src/index.js
@@ -5,8 +5,18 @@ const app = express();
 app.use(express.json());
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
+const corsUrl = [process.env.CLIENT_URL] || ["*"]
 
+// Cors
+app.use((req, res, next) => {
+    res.append("Access-Control-Allow-Origin", `${corsUrl}`);
+    res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    res.append('Access-Control-Allow-Credentials', true)
+    next()
+})
+
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Corriendo en el puerto ${PORT}`);
 })
