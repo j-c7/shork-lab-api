d075e2290d595ecffe4936f90f59f525346dc82c
7jcdev <7jaydev@protonmail.com>
2024-11-15 06:18:35 -0600

    Added environment variables

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import express from "express";
-
+import dotenv from "dotenv";
 
 const app = express();
 app.use(express.json());
+dotenv.config();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Corriendo en el puerto ${PORT}`);
